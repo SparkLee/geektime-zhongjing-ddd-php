636b30b3e9feba572fccd887ea2b2ec684d50b1f
SparkLee <liweijsj@163.com>
2023-02-23 21:53:37 +0800

    rename

--- app/Domain/OrgMng/Org/Validator/OrgNameValidator.php
+++ app/Domain/OrgMng/Org/Validator/OrgNameValidator.php
@@ -6,20 +6,20 @@ use App\Domain\Common\Exceptions\BusinessException;
 
 class OrgNameValidator
 {
-    public function verify(int $tenantId, int $superiorId, string $name)
+    public function verify(int $tenantId, int $superiorId, string $name): void
     {
-        $this->nameShouldNotEmpty($name);
-        $this->nameShouldNotDuplicatedInSameSuperior($tenantId, $superiorId, $name);
+        $this->nameShouldNotBeEmpty($name);
+        $this->nameShouldNotBeDuplicatedInTheSameSuperior($tenantId, $superiorId, $name);
     }
 
-    private function nameShouldNotEmpty(string $name)
+    private function nameShouldNotBeEmpty(string $name): void
     {
         if (empty($name)) {
             throw new BusinessException("组织名不能为空");
         }
     }
 
-    private function nameShouldNotDuplicatedInSameSuperior(int $tenantId, int $superiorId, string $name)
+    private function nameShouldNotBeDuplicatedInTheSameSuperior(int $tenantId, int $superiorId, string $name): void
     {
 
     }
