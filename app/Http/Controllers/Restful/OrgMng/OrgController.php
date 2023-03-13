dad650abaf1869a73e3db2d49f718d3dfbf4e34c
SparkLee <liweijsj@163.com>
2023-03-13 23:41:46 +0800

    extract method

--- app/Http/Controllers/Restful/OrgMng/OrgController.php
+++ app/Http/Controllers/Restful/OrgMng/OrgController.php
@@ -7,6 +7,7 @@ use App\Application\OrgMng\OrgService\OrgService;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class OrgController extends Controller
@@ -21,13 +22,7 @@ class OrgController extends Controller
     public function addOrg(Request $request): JsonResponse
     {
         try {
-            $this->validate($request, [
-                'tenant' => ['required', 'numeric', 'min:1'],
-            ], [
-                'tenant.required' => '租户不能为空',
-                'tenant.numeric' => '租户必须是一个数字',
-                'tenant.min' => '租户必须大于0',
-            ]);
+            $this->validateAddingOrg($request);
             $createOrgRequest = CreateOrgRequest::fromRequest($request);
             $orgResponse = $this->orgService->addOrg($createOrgRequest, 1);
             return response()->json($orgResponse->toArray());
@@ -36,8 +31,22 @@ class OrgController extends Controller
         }
     }
 
-    public function updateOrgBasicInfo()
+    public function updateOrgBasicInfo(): JsonResponse
     {
         return response()->json('success');
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function validateAddingOrg(Request $request): void
+    {
+        $this->validate($request, [
+            'tenant' => ['required', 'numeric', 'min:1'],
+        ], [
+            'tenant.required' => '租户不能为空',
+            'tenant.numeric' => '租户必须是一个数字',
+            'tenant.min' => '租户必须大于0',
+        ]);
     }
 }
